6f10d53a7b0ed97e3408d576632ee90f7b6b951a
Luis Carlos <luisca2992@gmail.com>
2021-03-02 19:54:27 -0600

    initial commit React Redux pitchIdea

---
+++ src/components/App/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const app = () =>{
+    return(
+        <div className="app">
+            PITCH IDEA
+        </div>
+    )
+};
+
+export default app;
